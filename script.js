6963906e837d9116adcdcce53e5cc7899d203534
AaronM524 <aaronmathew468@gmail.com>
2025-02-20 09:12:55 -0500

    added video section and borders

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("VE").addEventListener("click", function() {
-    window.open("video.html", "_blank");
+    window.open("video1.html", "_blank");
 });
-document.getElementById("MMS").addEventListener("click", function() {
-    window.open("mustangmorningshow.html", "_blank");
+document.getElementById("VE2").addEventListener("click", function() {
+    window.open("video2.html", "_blank");
 });
-document.getElementById("ID").addEventListener("click", function() {
-    window.open("courses.html", "_blank");
+document.getElementById("HE").addEventListener("click", function() {
+    window.open("index.html", "_blank");
 });
 document.getElementById("YT").addEventListener("click", function() {
     window.open("https://www.youtube.com/channel/UC9aXXl56PYVK-GSPISyCLQg?view_as=subscriber", "_blank");
